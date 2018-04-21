97bb2c170db3d43d9d4f21709a26dd6f85395cf5
michal992 <tiqer3k@gmail.com>
2018-04-21 08:34:25 +0200

    nowy

---
+++ src/components/Main.js
@@ -0,0 +1,29 @@
+
+import React from 'react';
+import Text from './Text';
+
+class AppComponent extends React.Component {
+  constructor(){
+    super();
+    this.state = { text: 'Not clicked!'};
+  }
+
+  onButtonClick(){
+    this.setState({ text: 'Clicked!'});
+  }
+
+
+  render() {
+    return (
+      <div className="main">
+        <Text staticText="Text from child component"
+              clickText={this.state.text} />
+        <p>{this.state.text}</p>
+        <button onClick={this.onButtonClick.bind(this)}>Click</button>
+      </div>
+    );
+  }
+}
+
+
+export default AppComponent;
